7f1e7a8840fdb6294568bf71bb0be59119e84230
Efornies <eleforbel@gmail.com>
2022-03-24 19:08:34 +0000

    cambio de archivo y denominacion de variables

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # El Loop eterno del For por Elena For.
 
+He realizado un loop de dominios, en el sentido que se generan automáticamente de manera aleatoria.
+
 Ejecuta con:
 
 ```
--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i in pronoun) {
+    for (let a in adj) {
+      for (let b in noun) {
+        console.log(pronoun[i] + adj[a] + noun[b] + ".com");
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
